20a0310e9bd7c8e4c5726e83c91761d79c5cc536
ioulasri <imad.oulasri@iu-study.org>
2023-02-16 12:56:16 +0100

    ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main : Entry point
+ * main - print sizes of data types
  * Description : prints various data types
  *  and their sizes
  * Return: 0 if worked, non-zero otherwise
