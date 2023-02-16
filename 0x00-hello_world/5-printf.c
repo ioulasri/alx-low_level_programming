c9c74687ffda8713fe2548562384a6b9a8797d47
ioulasri <imad.oulasri@iu-study.org>
2023-02-16 12:42:04 +0100

    ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints with proper grammar, 
+ * but the outcome is a piece of art,'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
