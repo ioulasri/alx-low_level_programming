6b02cc7dcea7298bf99e46c576f2af66ee291b84
ioulasri <imad.oulasri@iu-study.org>
2023-02-16 12:42:33 +0100

    ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'prints with proper grammar, 
+ * Description: 'prints with proper grammar,
  * but the outcome is a piece of art,'
  *
  * Return: Always 0 (Success)
