6d47a39806d6c0ac7e89cba8d6ee0aa7912ebd38
ioulasri <imad.oulasri@iu-study.org>
2023-02-18 16:49:29 +0100

    ok

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,30 @@
  */
 int main(void)
 {
-   	int i;
-   	int j;
+	int i;
+	int j;
 
-   	i = 0;
-   	while (i <= 99)
-   	{
-      	j = i + 1;
-      	while (j <= 99)
-      	{
-         	putchar(48 + i / 10);
-         	putchar(48 + i % 10);
-         	putchar(' ');
-         	putchar(48 + j / 10);
-         	putchar(48 + j % 10);
-         	if (i != 98 || j != 99)
-         	{
-            	putchar(',');
-            	putchar(' ');
-         	}
-         	j++;
-      	}
-      	i++;
-   	}
-   	putchar('\n');
-   	return (0);
+	i = 0;
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(48 + i / 10);
+			putchar(48 + i % 10);
+			putchar(' ');
+			putchar(48 + j / 10);
+			putchar(48 + j % 10);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
 
