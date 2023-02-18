20db41bdfa20621373c3716539bdff5a7c21bef2
ioulasri <imad.oulasri@iu-study.org>
2023-02-18 16:47:51 +0100

    ok

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,34 @@
  */
 int main(void)
 {
-   	int i;
-   	int j;
-   	int k;
+	int i;
+	int j;
+	int k;
 
-   	i = 0;
-   	while (i <= 7)
-   	{
-      	j = i + 1;
-      	while (j <= 8)
-      	{
-         	k = j + 1;
-         	while (k <= 9)
-         	{
-            	putchar(i + 48);
-            	putchar(j + 48);
-            	putchar(k + 48);
-            	if (i < 7 || j < 8 || k < 9)
-            	{
-               		putchar(',');
-               		putchar(' ');
-            	}
-            	k++;
-         	}
-         	j++;
-      	}
-      	i++;
-   	}
-   	putchar('\n');
-   	return (0);
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i < 7 || j < 8 || k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
 
