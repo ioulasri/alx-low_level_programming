cf6d707d61836187492bc459804c32e66d407759
ioulasri <imad.oulasri@iu-study.org>
2023-02-18 16:46:01 +0100

    ok

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-   	int i;
-   	int j;
+	int i;
+	int j;
 
-   	i = 0;
-   	while (i <= 8)
-   	{
-      	j = i + 1;
-      	while (j <= 9)
-      	{
-         	putchar(i + 48);
-         	putchar(j + 48);
-         	if (i < 8 || j < 9)
-         	{
-            	putchar(',');
-            	putchar(' ');
-         	}
-         	j++;
-      	}
-      	i++;
-   	}
-   	putchar('\n');
-   	return (0);
+	i = 0;
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
 
