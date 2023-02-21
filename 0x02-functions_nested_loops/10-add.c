8cd1c72f880b7230c9b2a14d224d46966fff2c51
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 19:01:13 +0100

    ok

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -13,17 +13,14 @@ void print_number(int nb)
 		nb = nb * (-1);
 		print_number(nb);
 	}
-	else if (nb > 10)
+	if (n >= 10)
 	{
-		print_number(nb / 10);
-		print_number(nb % 10);
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
 	}
 	else
-	{
-		_putchar(nb + 48);
-	}
+		_putchar(n + 48);
 }
-
 /**
  * add - adds two integers given as parameters
  * @a: int a the first integer
