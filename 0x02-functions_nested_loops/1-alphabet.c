0435f2cfc607a2781d1e4d5caa51e12c5ee27acd
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 14:31:27 +0100

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets
+ * description - print alphabets using _putchar in main.h
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}
+
