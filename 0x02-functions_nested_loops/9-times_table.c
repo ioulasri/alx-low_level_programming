c831a2d1e96e952798f64f1d5f1af14343265567
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 18:34:05 +0100

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,29 @@
 #include "main.h"
 
 /**
+ * check - check if it's the last column of each line.
+ * @i: int i the number to be multiplied
+ * @mult: int mult the number i is multiplied with
+ */
+
+void check(int i, int mult)
+{
+	if (mult < 9 && (i * (mult + 1)) >= 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	if (mult < 9 && (i * (mult + 1)) < 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+}
+
+/**
  * times_table - prints the 9 times table, starting with 0.
+ * print ', ' otherwise.
  * Return: always 0 (Success)
  */
 
@@ -14,42 +36,21 @@ void times_table(void)
 	i = 0;
 	while (i <= 9)
 	{
-        mult = 0;
+		mult = 0;
 		while (mult <= 9)
 		{
-		    a = i * mult;
-		    if (i * mult >= 10)
-		    {
-		        _putchar((a / 10) + 48);
-		        _putchar((a % 10) + 48);
-		        if (mult < 9 && (i * (mult + 1)) >= 10)
-    			{
-    			    _putchar(',');
-    			    _putchar(' ');
-    			}
-    			if (mult < 9 && (i * (mult + 1)) < 10)
-    			{
-    			    _putchar(',');
-    			    _putchar(' ');
-    			    _putchar(' ');
-    			}
-		    }
-		    else
-		    {
-		        _putchar(a + 48);
-		        if (mult < 9 && (i * (mult + 1)) >= 10)
-			    {
-			    _putchar(',');
-			    _putchar(' ');
-			    }
-			if (mult < 9 && (i * (mult + 1)) < 10)
-			    {
-			    _putchar(',');
-			    _putchar(' ');
-			    _putchar(' ');
-			    }
-		    }
-		
+			a = i * mult;
+			if (i * mult >= 10)
+			{
+				_putchar((a / 10) + 48);
+				_putchar((a % 10) + 48);
+				check(i, mult);
+			}
+			else
+			{
+				_putchar(a + 48);
+				check(i, mult);
+			}
 			mult++;
 		}
 		mult = 0;
