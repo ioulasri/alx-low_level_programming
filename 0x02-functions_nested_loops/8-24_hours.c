27efd5b2203207ab8449adcdc20b5a5906726d2a
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 17:50:37 +0100

    ok

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= 23)
+	{
+		b = 0;
+		while (b <= 59)
+		{
+			_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+			_putchar(':');
+			_putchar((b / 10) + 48);
+			_putchar((b / 10) + 48);
+			_putchar('\n');
+			b++;
+		}
+	a++;
+	}
+}
+
