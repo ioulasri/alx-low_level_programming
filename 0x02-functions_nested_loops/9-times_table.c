40d7678f0ae00f6bc3959bdf362346b83d5bcfd3
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 18:20:26 +0100

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,39 +9,51 @@ void times_table(void)
 {
 	int i;
 	int mult;
-	int result;
+	int a;
 
 	i = 0;
 	while (i <= 9)
 	{
-		mult = 0;
+        mult = 0;
 		while (mult <= 9)
 		{
-			result = i * mult;
-			if (i * mult >= 10)
-			{
-				_putchar((result / 10) + 48);
-				_putchar((result % 10) + 48);
-				if (mult < 9)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar(result + 48);
-				if (mult < 9)
-				{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				}
-			}
+		    a = i * mult;
+		    if (i * mult >= 10)
+		    {
+		        _putchar((a / 10) + 48);
+		        _putchar((a % 10) + 48);
+		        if (mult < 9 && (i * (mult + 1)) >= 10)
+    			{
+    			    _putchar(',');
+    			    _putchar(' ');
+    			}
+    			if (mult < 9 && (i * (mult + 1)) < 10)
+    			{
+    			    _putchar(',');
+    			    _putchar(' ');
+    			    _putchar(' ');
+    			}
+		    }
+		    else
+		    {
+		        _putchar(a + 48);
+		        if (mult < 9 && (i * (mult + 1)) >= 10)
+			    {
+			    _putchar(',');
+			    _putchar(' ');
+			    }
+			if (mult < 9 && (i * (mult + 1)) < 10)
+			    {
+			    _putchar(',');
+			    _putchar(' ');
+			    _putchar(' ');
+			    }
+		    }
+		
 			mult++;
 		}
 		mult = 0;
 		i++;
-		_putchar('\n');
+		putchar('\n');
 	}
 }
