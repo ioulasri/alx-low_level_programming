145fb2622f5bd363473168aead3c7c8cffbbe1af
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 19:09:46 +0100

    ok

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - prints the number given as parameter
- * @nb: number to be printed
- */
-
-void print_number(int nb)
-{
-	if (nb < 0)
-	{
-		_putchar('-');
-		nb = nb * (-1);
-		print_number(nb);
-	}
-	else if (nb > 10)
-	{
-		print_number(nb / 10);
-		print_number(nb % 10);
-	}
-	else
-	{
-		_putchar(nb + 48);
-	}
-}
-
-/**
  * add - adds two integers given as parameters
  * @a: int a the first integer
  * @b: int b the second integer
