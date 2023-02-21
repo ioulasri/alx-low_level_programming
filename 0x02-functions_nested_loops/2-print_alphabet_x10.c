10f331b1e7a9b4d7d0c20caa417a06c32b4a4bab
ioulasri <imad.oulasri@iu-study.org>
2023-02-21 15:16:32 +0100

    ok

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+
+ void print_alphabet_x10(void)
+ {
+ 	int i;
+
+	i = 0;
+	while(i <= 9)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+ }
+
