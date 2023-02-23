29c634087b47f9653b25ecf91d0ef6fa4f17e03a
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 15:01:18 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line n times long
+ * @n: number of lines
+ * description: prints a line n times long using _putchar
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 1;
+	if (n == 0)
+		_putchar('\n');
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
