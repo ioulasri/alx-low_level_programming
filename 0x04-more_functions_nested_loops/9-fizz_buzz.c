60f02393d68a7420215d70ed98f9dc95bef72d8a
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 16:04:34 +0100

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,15 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
+		else if (i == 1)
+			printf("%d", i);
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 		i++;
 	}
 	printf("\n");
