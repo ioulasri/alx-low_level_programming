1ced49f12b846020c2016e6b6c6ec0baf830e164
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 22:50:55 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int is_prime(long int n) {
+	if (n < 2) {
+		return 0;
+	}
+	long int i = 2;
+	while (i <= n / i) {
+		if (n % i == 0) {
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
+long int largest_prime_factor(long int n) {
+	long int largest_factor = 1;
+	long int i = 2;
+	while (i <= n / i) {
+		if (n % i == 0) {
+			if (is_prime(i)) {
+				largest_factor = i;
+			}
+			if (is_prime(n / i)) {
+				largest_factor = n / i;
+			}
+		}
+		i++;
+	}
+	return largest_factor;
+}
+
+int main() 
+{
+	long int n = 612852475143;
+	printf("Largest prime factor of %ld is %ld\n", n, largest_prime_factor(n));
+	return 0;
+}
