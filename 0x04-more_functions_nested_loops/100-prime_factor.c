667d8e768f29701aaa665a4106c1186caa4e1223
ioulasri <imad.oulasri@iu-study.org>
2023-02-24 00:55:57 +0100

    ok

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    long int i, n;
+	long int i, n;
 
-    i = 612852475143;
-    n = 2;
-    while (n <= i)
-    {
-        if (i % n == 0)
-        {
-            i = i / n;
-            n--;
-        }
-        n++;
-    }
-    printf("%lu\n", n);
-    return (0);
+	i = 612852475143;
+	n = 2;
+	while (n <= i)
+	{
+		if (i % n == 0)
+		{
+			i = i / n;
+			n--;
+		}
+		n++;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
