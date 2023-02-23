7f30bd2ab94965846f995568813e83214df70dfa
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 15:06:49 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a line n times long
+ * @n: number of lines
+ * description: prints a line n times long using _putchar
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	i = 1;
+	if (n == 0)
+		_putchar('\n');
+	else
+	{
+		while (i <= n)
+		{
+			_putchar(' ');
+			i++;
+		}
+		_putchar('\\')
+		_putchar('\n');
+	}
+}
