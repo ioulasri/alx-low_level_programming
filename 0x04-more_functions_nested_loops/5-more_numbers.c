8702701d11ad4e4b6252f66d18fd16bb10bb60a6
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 13:45:42 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 14
+ * description: prints numbers starting from 0 to 14
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	number = 0;
+	while (number <= 14)
+	{
+		if (number > 10)
+		{
+			_putchar(number / 10 + 48);
+			print_one_digit(number);
+		}
+		else
+			print_one_digit(number);
+		number++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_one_digit - prints one digit
+ * @n: the number to be printed
+ */
+
+void print_one_digit(int n)
+{
+	_putchar(n % 10 + 48);
+}
+
+/**
+ * more_numbers - prints numbers
+ * description: prints numbers starting from 0 to 14 10 timess
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 10)
+	{
+		print_numbers();
+	}
+}
