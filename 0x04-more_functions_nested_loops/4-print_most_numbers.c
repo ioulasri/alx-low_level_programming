52f3560ea459a4ba5e971c9e505afc5d2804d7a2
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 13:32:03 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ * description: prints number starting from 0 to 9 except for 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	char number;
+
+	number = '0';
+	while (number <= '9' && (number != '2' || number != '4'))
+	{
+		_putchar(number);
+		number++;
+	}
+	_putchar('\n');
+}
