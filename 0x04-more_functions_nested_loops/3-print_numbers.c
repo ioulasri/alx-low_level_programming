ea916ad2078b23d453398478977c3c413521749a
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 13:29:06 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 to 9
+ * description: prints number starting from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		_putchar(number);
+		number++;
+	}
+}
