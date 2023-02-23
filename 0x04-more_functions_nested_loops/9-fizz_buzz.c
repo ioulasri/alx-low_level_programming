6047433e9beab31d30a5dfe4079d1edfe587abec
ioulasri <imad.oulasri@iu-study.org>
2023-02-23 15:50:49 +0100

    ok

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desription: FIZZBUZZ who doesn't know it
+ */
+
+int main()
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+}
