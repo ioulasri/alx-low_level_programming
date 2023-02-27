c4b28a701a8166694cd07d3f8b4bce4b77e729a5
ioulasri <imad.oulasri@iu-study.org>
2023-02-27 13:41:22 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a every other char in a string starting from 1st
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i % 2 == 1)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
