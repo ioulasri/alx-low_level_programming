04ad4ae856a56b73f076758ed6cbe9eb8d74758d
ioulasri <imad.oulasri@iu-study.org>
2023-02-27 13:05:36 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
