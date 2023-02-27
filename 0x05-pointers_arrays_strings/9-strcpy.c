afb76e1631b5149d279935f122249c13d21f06b1
ioulasri <imad.oulasri@iu-study.org>
2023-02-27 14:06:11 +0100

    ok

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: the string to copy to
+ * @src: the string to copy from
+ * Return: dest the string the has src content
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
