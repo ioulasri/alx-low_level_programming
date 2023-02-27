9cd0cb7a5566ca27cbfa7e25e2b58cc335c76606
ioulasri <imad.oulasri@iu-study.org>
2023-02-27 13:32:40 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char c;
+
+	i = 0;
+	j = -1;
+	while (s[i])
+		i++;
+	while (j++ < i-- / 2)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+	}
+}
