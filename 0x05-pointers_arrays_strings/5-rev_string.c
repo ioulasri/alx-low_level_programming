9d4be390c612574440250db133389358a4835fa4
ioulasri <imad.oulasri@iu-study.org>
2023-02-27 13:38:39 +0100

    ok

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,20 @@ void rev_string(char *s)
 {
 	int i;
 	int j;
+	int len;
 	char c;
 
 	i = 0;
 	j = 0;
 	while (s[i])
 		i++;
-	i--;
+	len = i - 1;
 	while (j < i / 2)
 	{
 		c = s[j];
-		s[j] = s[i];
-		s[i] = c;
-		i--;
+		s[j] = s[len];
+		s[len] = c;
+		len--;
 		j++;
 	}
 }
