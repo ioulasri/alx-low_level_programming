767c1c628312655767999396dff3bbaa43350356
ioulasri <imad.oulasri@iu-study.org>
2023-03-01 22:26:53 +0100

    ok

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * cap_string - Captilizes the string given as parameter
@@ -11,6 +12,10 @@ char *cap_string(char *s)
 	int i;
 
 	i = 0;
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[0] -= 32;
+	}
 	while (s[i])
 	{
 		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
@@ -19,10 +24,20 @@ char *cap_string(char *s)
 			|| s[i] == '}')
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] -= 32;
+				s[i + 1] -= 'a' - 'A';
 		}
 
 		i++;
 	}
 	return (s);
+}
+
+int main(void)
+{
+    char str[] = "expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    return (0);
 }
