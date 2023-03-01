850c7272bbe9a034d871b6a1a04470b13570aad9
ioulasri <imad.oulasri@iu-study.org>
2023-03-01 13:29:54 +0100

    ok

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - Captilizes the string given as parameter
+ * @s: the string
+ * Return: the captilized string
+ **/
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+			|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+			|| s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+			|| s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] += 'A' - 'a';
+		}
+
+		i++;
+	}
+	return (s);
+}
