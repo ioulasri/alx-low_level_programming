692013ff00b9b51bb5380468b5039190f05fa82a
ioulasri <imad.oulasri@iu-study.org>
2023-03-02 18:44:31 +0100

    ok

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * leet - converts specific characters to 4-3-0-7-1
@@ -29,4 +30,15 @@ char *leet(char *s)
 		i++;
 	}
 	return (s);
+}
+
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
