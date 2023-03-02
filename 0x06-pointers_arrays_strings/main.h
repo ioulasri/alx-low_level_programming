f498297c21c6e2a6ce2b9eb7d46d072490300094
ioulasri <imad.oulasri@iu-study.org>
2023-03-02 15:47:22 +0100

    ok

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-char *infinite_add(char *num1, char *num2, char *result, int result_size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
