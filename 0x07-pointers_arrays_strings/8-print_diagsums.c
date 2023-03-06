e66b9c98f60e8f2c1a6f64a82044bb471bc6bbaa
ioulasri <imad.oulasri@iu-study.org>
2023-03-06 18:21:48 +0100

    ok

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - prints the sum of the two diagonals.
+ * @a: the 2d array
+ * @size: the size of the array
+ */
+
 void print_diagsums(int *a, int size)
 {
 	int i;
 	unsigned int sum_1;
 	unsigned int sum_2;
-	
+
 	i = 0;
 	sum_1 = 0;
 	sum_2 = 0;
@@ -18,23 +24,4 @@ void print_diagsums(int *a, int size)
 		i++;
 	}
 	printf("%d, %d\n",sum_1, sum_2);
-}
-
-int main(void)
-{
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
-    return (0);
 }
