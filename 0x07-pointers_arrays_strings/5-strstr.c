c102ebade002bb71f7cfb15f70106b41346a92e6
ioulasri <imad.oulasri@iu-study.org>
2023-03-06 18:44:09 +0100

    ok

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,70 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strncmp - compares the two string s1 and s2
- * @s1: the first string
- * @s2: the second string
- * @n: the len
- * Return: returns the difference between the two strings;
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
  */
-
-int _strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		if (i < n && (s1[i] != s2[i]))
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	if (i >= n)
-		return (0);
-	return (s1[i] - s2[i]);
-}
-
-/**
- * _strlen - returns the length of a given string
- * @s: the string
- * Return: the length of given string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strstr - locates a substring.
- * @haystack: string
- * @needle: subtring
- * Return: substring if found, NULL otherwise.
- */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int needle_len;
-	int haystack_len;
+	int i
+	int j;
 
-	needle_len = _strlen(needle);
-	haystack_len = _strlen(haystack);
-
-	if (needle_len > haystack_len)
-		return (NULL);
-
-	for (int i = 0; i <= haystack_len - needle_len; i++)
-	{
-		if (_strncmp(&haystack[i], needle, needle_len) == 0)
+	while (haystack[i])
+	{	
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			j++;
+		}
+		if (!needle[j])
 			return (&haystack[i]);
+		i++;
 	}
 	return (NULL);
 }
