0b2f3e14bddd8d5ef5481728df36815a99f331fd
ioulasri <imad.oulasri@iu-study.org>
2023-03-06 18:38:10 +0100

    ok

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * _strstr - locates a substring.
  * @haystack: string
  * @needle: subtring
@@ -10,32 +28,19 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-	int index;
-	int matching;
+	int needle_len;
+	int haystack_len;
 
-	i = 0;
-	while (haystack[i])
+	needle_len = _strlen(needle);
+	haystack_len = _strlen(haystack);
+
+	if (needle_len > haystack_len)
+		return (NULL);
+
+	for (int i = 0; i <= haystack_len - needle_len; i++)
 	{
-		matching = 1;
-		j = 0;
-		if (haystack[i] == needle[j])
-		{
-			index = i;
-			while (needle[j])
-			{
-				if (needle[j] != haystack[index])
-					matching = 0;
-				j++;
-				index++;
-			}
-			if (matching == 1)
-			{
-				return (&haystack[i]);
-			}
-		}
-		i++;
+		if (strncmp(&haystack[i], needle, needle_len) == 0)
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
