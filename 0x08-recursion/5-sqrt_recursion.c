5cdd0a180114c3de2620647f890e69983fef67df
ioulasri <imad.oulasri@iu-study.org>
2023-03-08 15:50:38 +0100

    ok

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * find_square_root - finds the square root with a binary algorithm
+ * @n: the number to find the square root of
+ * @low: the lower value of the range
+ * @high: the higher value of the range
+ * Return: return the square root of n, -1 otherwise.
+ */
+
+int find_square_root(int n, int low, int high)
+{
+	int mid;
+
+	mid = (low + high) / 2;
+	if (mid * mid == n)
+		return (mid);
+	else if (mid * mid < n && (mid + 1) * (mid + 1) > n)
+		return (-1);
+	else if (mid * mid > n)
+		return (find_square_root(n, low, mid));
+	else
+		return (find_square_root(n, mid + 1, high));
+}
+
+/**
+ * _sqrt_recursion - computes the square root of n 
+ * @n: the number to compute the square root of
+ * Return: return the square root of n, -1 otherwise.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (find_square_root(n, 0, n));
+}
