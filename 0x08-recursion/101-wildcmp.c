71f9852725bd6aa5f1c23bb1a76eecc3f20e6cc0
ioulasri <imad.oulasri@iu-study.org>
2023-03-08 16:58:26 +0100

    ok

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,15 +12,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
-	{
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
-		return (wildcmp(s1, s2 + 1));
-	}
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	if (*s1 == *s2 || *s2 == '*')
 		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
