3ee27e5837ace12f3fa944a390b6904f75257ca8
ioulasri <imad.oulasri@iu-study.org>
2023-03-08 07:04:25 +0100

    ok

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a given string
+ * @s: the string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	_putchar(s[i]);
+	s++;
+	if (s[i])
+		_puts_recursion(s);
+	else
+		_putchar('\n');
+}
