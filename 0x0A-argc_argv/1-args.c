864d95fb919178e7a08c9b62d283c634dc585d53
ioulasri <imad.oulasri@iu-study.org>
2023-03-10 15:55:22 +0100

    ok

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the number of arguments.
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ * Return: always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
