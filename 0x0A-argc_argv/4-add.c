941f07af082b9c16031c1e68638389187541a942
ioulasri <imad.oulasri@iu-study.org>
2023-03-10 16:25:58 +0100

    ok

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,116 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_str_number(char *c)
+{
+	int i;
+
+	i = 1;
+	if (!(c[0] == '-' || (c[0] >= '0' && c[0] <= '9')))
+		return (0);
+	while (c[i])
+	{
+		if (c[i] <= '0' && c[i] >= '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * str_len - calculate the length of a string
+ * @s: the string
+ * Return: return the length
+ */
+
+int str_len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _atoi - converts a string to int
+ * @s: the string to convert
+ * Return: return the value of the string converted
+ */
+
+int _atoi(char *s)
+{
+	int result;
+	int sign;
+	int i;
+	int len;
+
+	i = 0;
+	len = str_len(s);
+	sign = 1;
+	result = 0;
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	if (len > i)
+	{
+		if (sign == -1)
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result *= 10;
+				result -= s[i] - 48;
+				i++;
+			}
+		}
+		else
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result *= 10;
+				result += s[i] - 48;
+				i++;
+			}
+		}
+	}
+	return (result);
+}
+
+/**
+ * main - Entry point
+ * Description: prints the number of arguments.
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	i = 1;
+	while (i < argc)
+	{
+		if (!check_str_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		sum += _atoi(argv[i]);
+		i++;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
