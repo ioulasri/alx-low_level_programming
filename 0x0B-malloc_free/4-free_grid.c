b48a79a88fad8b935ad508f3836fecd7716b4eb4
ioulasri <imad.oulasri@iu-study.org>
2023-03-13 16:41:04 +0100

    ok

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,13 +9,11 @@
 
 void free_grid(int **grid, int height)
 {
-    int i, j;
+	int i;
 
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            free(grid[i][j]);
-        }
-        free(grid[i]);
-    }
-    free(grid);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
