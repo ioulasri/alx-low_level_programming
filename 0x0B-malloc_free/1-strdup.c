9fa7ae4b6a1befa8c2978f5d367410b16524eed9
ioulasri <imad.oulasri@iu-study.org>
2023-03-13 14:36:18 +0100

    ok

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -29,9 +29,7 @@ int _strlen(char *s)
 char *_create_array(unsigned int size)
 {
 	char *array;
-	unsigned int i;
 
-	i = 0;
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
