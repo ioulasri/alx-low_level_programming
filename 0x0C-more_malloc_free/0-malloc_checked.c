8e824c17c4b40d6891cd36968f12e9a9be9185f1
ioulasri <imad.oulasri@iu-study.org>
2023-03-16 15:19:24 +0100

    ok

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,41 +1,15 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <limits.h>
 #include "main.h"
 
 void *malloc_checked(unsigned int b)
 {
 	int *memory;
-	int m;
 
-	m = 98;
 	memory = malloc(b);
 	if (!memory)
 	{
-		memory = &m;
-		return (memory);
+		exit(98);
 	}
 	return (memory);
-}
-
-int main(void)
-{
-    char *c;
-    int *i;
-    float *f;
-    double *d;
-
-    c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
-    i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
-    f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
-    d = malloc_checked(INT_MAX);
-    printf("%p\n", (void *)d);
-    free(c);
-    free(i);
-    free(f);
-    free(d);
-    return (0);
 }
