d6a19873f61b75ae0a32cecb2d6eaac8a52f72a6
ioulasri <imad.oulasri@iu-study.org>
2023-03-20 14:13:45 +0000

    ok

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+/**
+ * init_dog - intiliaze a variable of type dog
+ * @d: the struct
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the owner's name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog1;
+}
