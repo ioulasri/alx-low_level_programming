c879e798b3228c62f8012455a9221101f93f9789
ioulasri <imad.oulasri@iu-study.org>
2023-03-20 15:39:53 +0000

    ok

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	if (!d->name)
+	if (d->name == NULL)
 		d->name = "(nil)";
-	if (!d->owner)
+	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
