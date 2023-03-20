df04b12d346f0b0c0ea8581cc456d58086469db1
ioulasri <imad.oulasri@iu-study.org>
2023-03-20 12:20:22 +0000

    ok

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of this program
+ * description: prints the name of the file
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
