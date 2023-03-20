95850044c0be7c01434a82f227dcb635d93efd86
ioulasri <imad.oulasri@iu-study.org>
2023-03-20 22:45:07 +0000

    ok

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,86 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _create_array - creates an array of chars.
+ * @size: the size of the memory to allocate.
+ * Return: the array created.
+ */
+
+char *_create_array(unsigned int size)
+{
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (!array)
+		return (NULL);
+	return (array);
+}
+
+/**
+ * _strdup - duplicates a given string.
+ * @str: the string to duplicate.
+ * Return: the array created.
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	new_str = _create_array(_strlen(str) + 1);
+	if (!new_str)
+		return (NULL);
+	while (i <= _strlen(str))
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	return (new_str);
+}
+
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (!new_dog->owner)
+	{
+		free(new_dog->owner);
+		return (NULL);
+	}
+	return(new_dog);
+}
