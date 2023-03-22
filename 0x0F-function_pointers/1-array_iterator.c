1df4ac4292e8c7ceb02a3c5e39193233d05bed1f
ioulasri <imad.oulasri@iu-study.org>
2023-03-22 12:39:54 +0000

    ok

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,10 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
-	if (!array || !size || !action)
+	if (!array || !action)
 		return;
 	while (i < size)
 	{
