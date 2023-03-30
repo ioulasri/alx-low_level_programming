16fbd19463acc12510bbc7c67b6cb0ee56304e32
ioulasri <imad.oulasri@iu-study.org>
2023-03-30 16:03:00 +0000

    Debugged and pushed print linked list file

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to be added to the list
+ *
+ * Return: address of the new element, or NULL if it failed
+ *
+ * Description: This function creates a new node with a copy of the string
+ * 'str', and adds it to the end of the list pointed to by 'head'. If 'head'
+ * is NULL, or memory allocation fails, the function returns NULL. The string
+ * 'str' is duplicated using the 'strdup' function. The function returns a
+ * pointer to the new node.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(str);
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+	return (new_node);
+}
