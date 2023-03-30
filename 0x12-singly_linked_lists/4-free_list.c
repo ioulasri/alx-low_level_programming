3e722d0e02e5b305963470987f9127477086c4a2
ioulasri <imad.oulasri@iu-study.org>
2023-03-30 23:20:47 +0000

    Debugged and pushed print linked list file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ *
+ * Description: This function takes a pointer to the head of a list and frees
+ * all the nodes in the list. The memory occupied by each node is deallocated
+ * using the `free` function. The function does not return a value.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
