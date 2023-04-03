5f5fd970ef8a17a269ae580b4781900e5a23e112
ioulasri <imad.oulasri@iu-study.org>
2023-04-03 20:45:42 +0000

    Debugged and pushed the 7-get_nodeint.c program file

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
     listint_t *cur_node;
 
     cur_node = head;
-	while (index-- && cur_node->next)
+	while (index-- && cur_node)
 		cur_node = cur_node->next;
 	return (cur_node);
 }
