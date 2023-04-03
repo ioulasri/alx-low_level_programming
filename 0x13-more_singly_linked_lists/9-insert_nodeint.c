77af57200b84d0a2f525051cd107c51872d61216
ioulasri <imad.oulasri@iu-study.org>
2023-04-03 20:42:14 +0000

    Debugged and pushed the 9-insert_nodeint.c program file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	unsigned int i;
 
-	if (!*head)
+	if (!head)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
