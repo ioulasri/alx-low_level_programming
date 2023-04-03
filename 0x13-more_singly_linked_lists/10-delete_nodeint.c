6f4b342125346e31c90a0ffd11feea8706786392
ioulasri <imad.oulasri@iu-study.org>
2023-04-03 20:29:26 +0000

    Debugged and pushed the 10-delete_nodeint.c program file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (i = 0; i < index - 1 && temp; i++)
+	for (i = 0; i < index - 1 && temp->next; i++)
 		temp = temp->next;
 	if (!temp->next)
 	{
