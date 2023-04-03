283c982a80f2f8153ff536b5de068ec1a345e834
ioulasri <imad.oulasri@iu-study.org>
2023-04-03 13:17:23 +0000

    Debugged and pushed the 4-free_listint.c program file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked list and sets the head pointer to NULL.
+ * 
+ * @head: A pointer to the head of the linked list.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = head;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
