9f74d46d4f5fae68e50d163e779050d8ca22519d
ioulasri <imad.oulasri@iu-study.org>
2023-04-09 16:00:07 +0000

    Debugged and pushed the 5-flip_bits.c program file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits needed
+ * to flip to get from one number to another
+ *
+ * @n: first input unsigned long int
+ * @m: second input unsigned long int
+ *
+ * Return: count of bits needed to flip to get from n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned long int xor_res = n ^ m; 
+    unsigned int count = 0;
+
+    while (xor_res != 0)
+    {
+        if (xor_res & 1) 
+            count++;
+        xor_res >>= 1;
+    }
+    return count;
+}
