86287cd8acef2683fb56d3d8858fd74cc039afc7
ioulasri <imad.oulasri@iu-study.org>
2023-04-10 21:01:52 +0000

    Debugged and pushed the 0-read_textfile.c program file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,63 +1,44 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, or 0 if an error occurs
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL) {
-        return 0;
-    }
-    
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
-    char *buffer = malloc(letters);
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
 
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
-
-    ssize_t total_bytes_read = 0;
-    ssize_t bytes_read;
-
-    while (total_bytes_read < letters)
-    {
-        bytes_read = read(fd, buffer + total_bytes_read, letters - total_bytes_read);
-        if (bytes_read == 0)
-            break;
-        else if (bytes_read == -1) 
-        {
-            free(buffer);
-            close(fd);
-            return 0;
-		}
-        else
-            total_bytes_read += bytes_read;
-    }
-
-    if (write(STDOUT_FILENO, buffer, total_bytes_read) != total_bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
-    free(buffer);
-    close(fd);
-    return total_bytes_read;
-}
-
-int main(int ac, char **av)
-{
-    ssize_t n;
-
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (n_written);
 }
