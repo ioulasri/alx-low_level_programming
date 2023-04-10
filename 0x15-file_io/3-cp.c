de0ecbcab5dc27a171ae234cacb9c699673e332e
ioulasri <imad.oulasri@iu-study.org>
2023-04-10 21:15:56 +0000

    Debugged and pushed the 3-cp.c program file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,8 +18,10 @@ int main(int argc, char *argv[])
 
 	from_fd = open(argv[1], O_RDONLY);
 	if (from_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+		exit(98);
+	}
 	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -30,10 +32,11 @@ int main(int argc, char *argv[])
 		if (write_status == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
 	if (read_status == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+		exit(98);
+	}
 	if (close(from_fd) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd), exit(100);
 
