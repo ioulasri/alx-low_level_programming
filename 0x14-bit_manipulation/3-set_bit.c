858abdeeb044faa283e7cb89ff58b0e6cc7e2f81
imad <imad.oulasri@iu-study.org>
2023-05-04 16:44:23 +0100

    Created a function that sets the bit at a given index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	*n |= (1UL << index);
 
-	return 1;
+	return (1);
 }
