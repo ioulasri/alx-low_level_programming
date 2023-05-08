c37ad4a23ee672d98816b3691e208d08d01b4c6b
imad <imad.oulasri@iu-study.org>
2023-05-08 13:28:06 +0100

    Task 0 Solved

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints
+ * its contents to standard output.
+ *
+ * @filename: A pointer to a null-terminated string
+ * containing the name of the file to be read.
+ *
+ * @letters: The maximum number of bytes to be read from the file.
+ *
+ * Return: If the function succeeds, it returns
+ * the number of bytes read from the file and printed to
+ * the standard output. If an error occurs, it returns 0.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t re, wr, op;
