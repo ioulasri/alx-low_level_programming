fa44bd32f642ddef4c6a6ede6568fa304dec64d5
imad <imad.oulasri@iu-study.org>
2023-06-15 16:37:16 +0100

    adding a node at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,10 +39,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
+	{
+		free(new_node);
 		return (add_dnodeint(h, n));
-
+	}
 	if (idx == dlistint_len(*h) - 1)
+	{
+		free(new_node);
 		return (add_dnodeint_end(h, n));
+	}
 
 	curr = *h;
 	new_node->next = NULL;
