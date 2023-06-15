2e850ced07081009883dca3f621f8abfbc12d033
imad <imad.oulasri@iu-study.org>
2023-06-15 13:25:23 +0100

    adding a node to the beginning of a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
-		return (NULL);
-
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
