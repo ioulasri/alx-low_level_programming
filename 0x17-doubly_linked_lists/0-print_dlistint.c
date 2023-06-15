9ef47c749a04f56a316f2e3be73f4159317d3bb0
imad <imad.oulasri@iu-study.org>
2023-06-15 13:07:09 +0100

    printing data of doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *curr = h;
+    size_t size = 0;
+
+    if (!curr)
+        return (0);
+    while (curr)
+    {
+        printf("%d\n", curr->n);
+        curr = curr->next;
+        size++;
+    }
+    return (size);
+}
