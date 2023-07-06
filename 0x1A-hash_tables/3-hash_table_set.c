a8250bde67cd149b132fb17e22fbbe5636045325
imad <imad.oulasri@iu-study.org>
2023-07-06 13:41:18 +0100

    adding a key and value to the hashtable after hashing it

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,30 +11,30 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int size, index;
-    hash_node_t *node;
+	unsigned long int size, index;
+	hash_node_t *node;
 
-    if (strlen(key) == 0)
-        return (0);
+	if (strlen(key) == 0)
+		return (0);
 
-    size = ht->size;
-    index = key_index((unsigned const char*)key, size);
+	size = ht->size;
+	index = key_index((unsigned const char*)key, size);
 
-    node = malloc(sizeof(hash_node_t));
-    if (!node)
-        return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
 
-    node->key = (char *)key;
-    node->value = strdup(value);
-    node->next = NULL;
+	node->key = (char *)key;
+	node->value = strdup(value);
+	node->next = NULL;
 
-    if (!ht->array[index])
-    {
-        ht->array[index] = node;
-        return (1);
-    }
-    node->next = ht->array[index];
-    ht->array[index] = node;
-    return (1);
+	if (!ht->array[index])
+	{
+		ht->array[index] = node;
+		return (1);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
 
