5db4e96d85d838755aee22d72d649c4c1b875362
imad <imad.oulasri@iu-study.org>
2023-07-08 10:29:51 +0100

    fixed some problems

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,27 +9,23 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int size, index = 0;
+	unsigned long int size, index = 0, flag = 0;
 	hash_node_t *current;
-	int flag = 0;
 
 	if (!ht)
 		return;
-
 	size = ht->size;
+
 	printf("{");
 	while (index < size)
 	{
 		current = ht->array[index];
-		while (current != NULL)
+		while (current)
 		{
-			if (current->key)
-			{
-				if (flag)
-					printf(", ");
-				printf("%s: %s", current->key, current->value);
-				flag = 1;
-			}
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			flag = 1;
 			current = current->next;
 		}
 		index++;
