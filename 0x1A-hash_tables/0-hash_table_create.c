861132bda607ca5c899295c2814388d9fe432e23
imad <imad.oulasri@iu-study.org>
2023-07-08 10:23:15 +0100

    creating a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 
 	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(char *) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash_table->array)
+	{
+		free(hash_table->array);
 		return (NULL);
+	}
 	return (hash_table);
 }
+
